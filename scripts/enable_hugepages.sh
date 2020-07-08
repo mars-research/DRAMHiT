ad0979c20bef596d0682302386f6ceffb66afff4
Vikram Narayanan <vikram186@gmail.com>
2020-07-08 13:32:40 -0400

    Allocate more 1G pages

--- scripts/enable_hugepages.sh
+++ scripts/enable_hugepages.sh
@@ -8,13 +8,19 @@ echo always > /sys/kernel/mm/transparent_hugepage/defrag
 # https://www.kernel.org/doc/html/latest/admin-guide/mm/hugetlbpage.html
 
 # Dynamically reserve 1GB pages (40GBs on node0)
-echo 40 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
-echo 40 > /sys/devices/system/node/node1/hugepages/hugepages-1048576kB/nr_hugepages
+echo 80 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages
+echo 80 > /sys/devices/system/node/node1/hugepages/hugepages-1048576kB/nr_hugepages
 
 if [ ! -d /mnt/huge ]; then
 	sudo mkdir -p /mnt/huge
 fi
+
 sudo mount -t hugetlbfs nodev /mnt/huge
+
+USER=$(whoami)
+GROUP=$(id -g -n)
+
+sudo chown -R ${USER}:${GROUP} /mnt/huge
 
 # Check how many pages are allocated
 echo "2MiB pages: $(cat /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages)"
