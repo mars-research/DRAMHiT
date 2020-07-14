d79a55fd259e294443a37885df6f7b26ccad1af8
Harishankar Vishwanathan <hvishwan@uci.edu>
2020-07-13 22:12:31 -0500

    add fastq download scripts

---
+++ scripts/dataset_download/download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for filename in ./*.txt; do
+  echo "downloading files from $filename, 8 files in parallel"
+  basename=${filename##*/}
+  basename="${basename%.txt}"
+  echo "$basename"
+  if [ ! -d ./$basename ]; then
+	  mkdir -p ./$basename
+  fi
+  cat $f | xargs -n 1 -P 8 wget -q
+done
