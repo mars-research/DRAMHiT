3117f9947f063770bfbba80ca5a8bf6a670e196c
Vikram Narayanan <vikram186@gmail.com>
2021-01-11 22:10:48 -0500

    Add prefetch control script

---
+++ scripts/prefetch_control.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "usage: prefetch_control.sh <on|off>";
+  echo " on -> you should see 0x0";
+  echo " off -> you should see 0xf";
+fi
+
+if [ "$1" == "on" ]; then
+  echo "Turning on all prefetchers";
+  sudo wrmsr 0x1a4 -a 0x0
+  sudo rdmsr 0x1a4
+elif [ "$1" == "off" ]; then
+  echo "Turning off all prefetchers";
+  sudo wrmsr 0x1a4 -a 0xf
+  sudo rdmsr 0x1a4
+fi
+
