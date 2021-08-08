c5ef459b7e66d6bc26838c4dd2299a1af9e83ae6
David Detweiler <daviddetweiler1@gmail.com>
2021-08-08 16:49:46 -0400

    Add async find test

--- auto-tests/find_inserted.cpp
+++ auto-tests/find_inserted.cpp
@@ -1,4 +1,5 @@
-#include <string>
+#include <iostream>
+#include <string_view>
 
 #include "hashtable.h"
 #include "hashtables/cas_kht.hpp"
@@ -6,21 +7,33 @@
 
 namespace kmercounter {
 namespace {
-bool try_test(BaseHashTable* ht) {
+// Tests finds of inserted elements after a flush is forced
+// Avoiding asynchronous effects
+bool try_synchronous_test(BaseHashTable* ht) {
   try {
+    std::cerr << "[TEST] Synchronous\n";
+
     constexpr auto size = 1 << 8;
+    static_assert(size % HT_TESTS_BATCH_LENGTH == 0,
+                  "Test size is assumed to be a multiple of batch size");
+
+    static_assert(HT_TESTS_FIND_BATCH_LENGTH == HT_TESTS_BATCH_LENGTH,
+                  "Test logic assumes these batch sizes are equal");
+
+    std::uint64_t n_inserted{};
+    std::uint64_t n_found{};
+    std::uint64_t count{};
     for (std::uint64_t i{}; i < size; i += HT_TESTS_BATCH_LENGTH) {
-      static_assert(HT_TESTS_FIND_BATCH_LENGTH == HT_TESTS_BATCH_LENGTH,
-                    "Test logic assumes these batch sizes are equal");
+      ++count;
 
       std::array<Keys, size> keys{};
       std::array<Values, HT_TESTS_FIND_BATCH_LENGTH> values{};
       ValuePairs found{0, values.data()};
-
       for (std::uint64_t j{}; j < HT_TESTS_BATCH_LENGTH; ++j)
         keys.at(j) = {i + j, (i + j) * 2};
 
       KeyPairs items{HT_TESTS_BATCH_LENGTH, keys.data()};
+      n_inserted += items.first;
       ht->insert_batch(items);
       ht->flush_insert_queue();
       ht->find_batch(items, found);
@@ -30,11 +43,77 @@ bool try_test(BaseHashTable* ht) {
       }
 
       ht->flush_find_queue(found);
-      if (found.first != HT_TESTS_FIND_BATCH_LENGTH) {
-        std::cerr << "[TEST] Not all inserted values were found\n";
-        std::cerr << "[TEST] Found " << found.first << "\n";
-        return false;
-      }
+      n_found += found.first;
+    }
+
+    std::cerr << "[TEST] Ran " << count << " iterations\n";
+    if (n_found != n_inserted) {
+      std::cerr << "[TEST] Not all inserted values were found\n";
+      std::cerr << "[TEST] Found " << n_found << "!=" << n_inserted << "\n";
+      return false;
+    }
+  } catch (...) {
+    std::cerr << "Exception thrown\n";
+    return false;
+  }
+
+  return true;
+}
+
+bool try_asynchronous_test(BaseHashTable* ht) {
+  try {
+    std::cerr << "[TEST] Asynchronous\n";
+
+    constexpr auto size = 1 << 8;
+    static_assert(size % HT_TESTS_BATCH_LENGTH == 0,
+                  "Test size is assumed to be a multiple of batch size");
+
+    static_assert(HT_TESTS_FIND_BATCH_LENGTH == HT_TESTS_BATCH_LENGTH,
+                  "Test logic assumes these batch sizes are equal");
+
+    std::uint64_t n_inserted{};
+    std::uint64_t n_insert_loops {};
+    for (std::uint64_t i{}; i < size; i += HT_TESTS_BATCH_LENGTH) {
+      ++n_insert_loops;
+      std::array<Keys, HT_TESTS_BATCH_LENGTH> keys{};
+      std::array<Values, HT_TESTS_FIND_BATCH_LENGTH> values{};
+      ValuePairs found{0, values.data()};
+
+      for (std::uint64_t j{}; j < HT_TESTS_BATCH_LENGTH; ++j)
+        keys.at(j) = {i + j, (i + j) * 2};
+
+      KeyPairs items{HT_TESTS_BATCH_LENGTH, keys.data()};
+      ht->insert_batch(items);
+      n_inserted += HT_TESTS_BATCH_LENGTH;
+    }
+
+    ht->flush_insert_queue();
+
+    std::array<Values, HT_TESTS_FIND_BATCH_LENGTH> values{};
+    ValuePairs found{0, values.data()};
+    std::uint64_t n_found{};
+    std::uint64_t n_find_loops {};
+    for (std::uint64_t i{}; i < size; i += HT_TESTS_BATCH_LENGTH) {
+      ++n_find_loops;
+      std::array<Keys, HT_TESTS_BATCH_LENGTH> keys{};
+
+      for (std::uint64_t j{}; j < HT_TESTS_BATCH_LENGTH; ++j)
+        keys.at(j) = {i + j, (i + j) * 2};
+
+      KeyPairs items{HT_TESTS_BATCH_LENGTH, keys.data()};
+      ht->find_batch(items, found);
+      n_found += found.first;
+      found.first = 0;
+    }
+
+    ht->flush_find_queue(found);
+    n_found += found.first;
+    std::cerr << "[TEST] Ran " << n_insert_loops << " insert iterations\n";
+    std::cerr << "[TEST] Ran " << n_find_loops << " find iterations\n";
+    if (n_found != n_inserted) {
+      std::cerr << "[TEST] Not all inserted values were found\n";
+      std::cerr << "[TEST] Found " << n_found << " != " << n_inserted << "\n";
+      return false;
     }
   } catch (...) {
     std::cerr << "Exception thrown\n";
@@ -47,18 +126,38 @@ bool try_test(BaseHashTable* ht) {
 }  // namespace kmercounter
 
 int main(int argc, char** argv) {
-  if (argc != 2) return 0;
+  if (argc != 3) return 1;
 
   constexpr auto size = 1ull << 26;
-  const std::string type{argv[1]};
-  if (type == "partitioned")
-    return !kmercounter::try_test(
-        new kmercounter::PartitionedHashStore<kmercounter::Aggr_KV,
-                                              kmercounter::ItemQueue>{size, 0});
-  else if (type == "cas")
-    return !kmercounter::try_test(
-        new kmercounter::CASHashTable<kmercounter::Aggr_KV,
-                                      kmercounter::ItemQueue>{size});
+  const std::string_view type{argv[1]};
+  const std::string_view test{argv[2]};
 
-  return 1;
+  const auto run_test = [test]() -> bool (*)(kmercounter::BaseHashTable*) {
+    if (test == "sync")
+      return kmercounter::try_synchronous_test;
+    else if (test == "async")
+      return kmercounter::try_asynchronous_test;
+    else
+      return nullptr;
+  }();
+
+  const auto ht = [type]() -> kmercounter::BaseHashTable* {
+    if (type == "partitioned")
+      return new kmercounter::PartitionedHashStore<kmercounter::Aggr_KV,
+                                                   kmercounter::ItemQueue>{size,
+                                                                           0};
+    else if (type == "cas")
+      return new kmercounter::CASHashTable<kmercounter::Aggr_KV,
+                                           kmercounter::ItemQueue>{size};
+    else
+      return nullptr;
+  }();
+
+  if (!(ht && run_test)) {
+    std::cerr << "[TEST] Invalid test type\n";
+    std::cerr << "[TEST] " << type << " " << test << "\n";
+    return 1;
+  }
+
+  return !run_test(ht);
 }
