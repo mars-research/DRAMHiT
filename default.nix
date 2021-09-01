8282439118e3ca7708fa335fc2687705699e8c83
Tianjiao Huang <git@tjhu.dev>
2021-08-31 20:19:07 -0400

    add nix packaging and dont download gtest

---
+++ default.nix
@@ -0,0 +1,38 @@
+let
+  pinnedPkgs = import (import ./nixpkgs.nix) {};
+in {
+  pkgs ? pinnedPkgs,
+}: let
+  lib = pkgs.lib;
+  stdenv = pkgs.stdenv;
+in stdenv.mkDerivation {
+  name = "kvstore";
+  version = "0.1.0";
+
+  src = lib.cleanSourceWith {
+    filter = name: type: !(type == "directory" && baseNameOf name == "build");
+    src = lib.cleanSourceWith {
+      filter = lib.cleanSourceFilter;
+      src = ./.;
+    };
+  };
+
+  nativeBuildInputs = with pkgs; [ 
+    cmake
+    ninja
+    libarchive
+  ];
+
+  buildInputs = with pkgs; [
+    numactl
+    zlib
+    boost
+    gtest
+  ];
+  
+  installPhase = ''
+    mkdir -p $out/bin
+    cp kvstore $out/bin/
+  '';
+}
+
--- shell.nix
+++ shell.nix
@@ -1,8 +1,5 @@
 let
-  nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/f641b66ceb34664f4b92d688916472f843921fd3.tar.gz";
-    sha256 = "1hglx3c5qbng9j6bcrb5c2wip2c0qxdylbqm4iz23b2s7h787qsk";
-  };
+  nixpkgs = import ./nixpkgs.nix;
   pkgs = import nixpkgs {
     overlays = [
     ];
@@ -15,6 +12,7 @@ in pkgs.mkShell {
     numactl
     zlib
     boost
+    gtest
 
     gdb
     linuxPackages.perf
@@ -24,6 +22,5 @@ in pkgs.mkShell {
   ];
   nativeBuildInputs = with pkgs; [
     gcc11
-    pkg-config 
   ];
 }
