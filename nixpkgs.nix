6d893ee48bb76586badf34bb5682d718bbeec981
Tianjiao Huang <git@tjhu.dev>
2021-12-01 22:21:18 +0000

    nix: we deprecated nixpkgs.nix

--- nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/f641b66ceb34664f4b92d688916472f843921fd3.tar.gz";
-  sha256 = "1hglx3c5qbng9j6bcrb5c2wip2c0qxdylbqm4iz23b2s7h787qsk";
-}
