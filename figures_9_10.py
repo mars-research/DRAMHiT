af58c03f31f9ae4ed045285d53835bd6098566fc
David Detweiler <daviddetweiler1@gmail.com>
2021-12-19 22:42:26 -0500

    Checkpoint graph config

--- figures_9_10.py
+++ figures_9_10.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
 
     print('Building bq', flush=True)
     bq_home.mkdir(parents=True)
-    run_cmake(bq_home, source, ['-DPREFETCH=ON', '-DBRANCH=simd', '-DBQ_ZIPFIAN=ON'])
+    run_cmake(bq_home, source, ['-DPREFETCH=ON', '-DBRANCH=simd', '-DBQUEUE=ON', '-DBQ_ZIPFIAN=ON'])
 
     points = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
     for n in points:
