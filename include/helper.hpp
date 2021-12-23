42857caac48b5f06b9e452a65250224b33d78446
Vikram Narayanan <vikram186@gmail.com>
2021-12-23 01:04:55 -0500

    Move helper functions to a nested ns

--- include/helper.hpp
+++ include/helper.hpp
@@ -1,11 +1,18 @@
-#ifndef __HELPER_HPP__
-#define __HELPER_HPP__
+#pragma once
+
+#include <cstdlib>
 
 namespace kmercounter {
+namespace utils {
+
+constexpr void *zero_aligned_alloc(std::size_t alignment, std::size_t size) {
+  void *mem = std::aligned_alloc(alignment, size);
+  memset(mem, 0x0, size);
+  return mem;
+}
 constexpr uint64_t next_pow2(uint64_t v) {
   return (1ULL << (64 - __builtin_clzl(v - 1)));
 }
 
+}  // namespace utils
 }  // namespace kmercounter
-
-#endif  // __HELPER_HPP__
