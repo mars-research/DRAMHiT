3ac9941d12ae7046e5da445554df1467b1c3259a
Vikram Narayanan <vikram186@gmail.com>
2021-12-31 16:11:51 -0500

    Add msr-safe handler

--- scripts/enable_msr_safe.sh
+++ scripts/enable_msr_safe.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+USER=${SUDO_USER}
+
+if [[ ${USER} == "" ]]; then
+  USER=$(id -u -n)
+fi
+
+if [[ ${SUDO_GID} == "" ]]; then
+  GROUP=$(id -g -n)
+else
+  GROUP=$(getent group  | grep ${SUDO_GID} | cut -d':' -f1)
+fi
+
+echo ${USER}:${GROUP}
+sudo rmmod msr-safe
 pushd ../tools/msr-safe
 make && \
 sudo insmod msr-safe.ko && \
-sudo sh -c "cat allowlists/al_kvstore > /dev/cpu/msr_allowlist"
+sudo sh -c "cat allowlists/al_kvstore > /dev/cpu/msr_allowlist" && \
+sudo chown ${USER}:${GROUP} /dev/cpu/*/msr_safe
 popd
