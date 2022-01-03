9b50a33331fc97ebeb07fadf5965bd6dfafac09e
Tianjiao Huang <git@tjhu.dev>
2022-01-03 02:50:15 -0500

    impl MVP csv readerr

--- include/input_reader/csv.hpp
+++ include/input_reader/csv.hpp
@@ -1,6 +1,7 @@
 #ifndef INPUT_READER_CSV_HPP
 #define INPUT_READER_CSV_HPP
 
+#include <fstream>
 #include <variant>
 
 #include "input_reader_base.hpp"
@@ -12,26 +13,40 @@ namespace input_reader {
 using Field = std::variant<uint8_t, uint16_t, uint32_t, uint64_t, float, double>;
 
 /// A row in a table.
-// TODO: maybe a more compact representation to improve performance.
+// TODO: support other datatype
 class Row {
 public:
-  std::vector<Field> fields;
+  std::vector<uint64_t> fields;
 };
 
-/// Sequentially incrementin counter.
-template<class T>
-class CsvReader : public InputReader<T> {
+/// Read a CSV file.
+/// All data are cached in memory to save I/O.
+/// WIP; only reads the first integer column at this moment. 
+class CsvReader : public InputReader<uint64_t> {
     public:
-    CsvReader(std::string_view filename) {
+    CsvReader(std::string_view filename, std::string_view delimiter) {
+      // Read CSV line by line into memory.
+      std::fstream ifile(filename.data());
+      for (std::string line; std::getline(ifile, line); /*noop*/) {
+        const std::string field_str = line.substr(0, line.find(delimiter));
+        const uint64_t field = std::stoull(field_str);
+        data.push_back(field);
+      }
 
+      this->iter = this->data.begin();
     }
 
-    std::optional<T> next() override {
-        return data++;
+    std::optional<uint64_t> next() override {
+        if (this->iter == this->data.end()) {
+          return std::nullopt;
+        } else {
+          return *(this->iter++);
+        }
     }
 
 private:
-    T data;
+    std::vector<uint64_t> data;
+    std::vector<uint64_t>::iterator iter;
 };
 
 } // namespace input_reader
