22d4be2f34d99a58f539908cf1b2f763ac4362b0
Tianjiao Huang <git@tjhu.dev>
2022-01-06 14:43:58 -0500

    cleanup hashmap_test

--- auto-tests/hashmap_test.cpp
+++ auto-tests/hashmap_test.cpp
@@ -5,11 +5,11 @@
 #include <gtest/gtest.h>
 #include <absl/flags/flag.h>
 #include <absl/flags/parse.h>
+#include <plog/Log.h>
 
 #include "hashtable.h"
 #include "hashtables/cas_kht.hpp"
 #include "hashtables/simple_kht.hpp"
-#include "logger.h"
 
 /*
   - Fill depends on the *distinct* key/id pairs
@@ -27,22 +27,16 @@ namespace kmercounter {
 namespace {
 // Test names.
 const char NO_PREFETCH_TEST[] = "No prefetch";
-const char ASYNCHRONOUS_TEST[] = "Asynchronous";
-const char FILL_SYNC_TEST[] = "Fill Sync";
-const char UNIT_FILL_TEST[] = "Unit Fill";
-const char OFF_BY_ONE_TEST[] = "Off By One";
+const char SIMPLE_BATCH_QUERY_TEST[] = "Simple batch query";
 constexpr const char* TEST_FNS [] {
   NO_PREFETCH_TEST,
-  // ASYNCHRONOUS_TEST,
-  // FILL_SYNC_TEST,
-  // UNIT_FILL_TEST,
-  OFF_BY_ONE_TEST,
+  SIMPLE_BATCH_QUERY_TEST,
 };
 // Hashtable names.
-const char PARTITIONED_CAS_HT[] = "Partitioned CAS";
-const char CAS_HT[] = "CAS";
+const char PARTITIONED_HT[] = "Partitioned HT";
+const char CAS_HT[] = "CAS HT";
 constexpr const char* HTS [] {
-  PARTITIONED_CAS_HT,
+  PARTITIONED_HT,
   CAS_HT,
 };
 
@@ -68,37 +62,20 @@ void no_prefetch_test(BaseHashTable* ht) {
   }
 }
 
-// Test for presence of an off-by-one error in synchronous use
-void single_insert_test(BaseHashTable* ht) {
-  Keys pair{1, 128};
-  KeyPairs keys{1ull, &pair};
-  ht->insert_batch(keys);
-  ht->flush_insert_queue();
-  std::cerr << "[TEST] Fill was: " << ht->get_fill() << "\n";
-  ASSERT_EQ(ht->get_fill(), 1);
-}
-
-// Test demonstrating the nonintuitive difference in the interpretation of batch
-// lengths between find/insert
-// NOTE: also noted a very strange use of the value field
-void off_by_one_test(BaseHashTable* ht) {
-  
+void simple_batch_query_test(BaseHashTable* ht) {
+  // Insertion.
   std::array<Keys, 2> keys{Keys{1, 128}, Keys{2, 256}};
   KeyPairs keypairs{2, keys.data()};
   ht->insert_batch(keypairs); 
   ht->flush_insert_queue();
 
-
+  // Look up.
   std::array<Values, HT_TESTS_BATCH_LENGTH> values{};
   ValuePairs valuepairs{0, values.data()};
-  
   ht->find_batch(keypairs, valuepairs);
-  
   ht->flush_find_queue(valuepairs);
-  std::cerr << "[TEST] Found " << valuepairs.first << ": "
-            << valuepairs.second[0] << std::endl;
 
-  // Note that we only insert 256 *once*, so the "value" should be 1
+  // Check for correctness.
   EXPECT_EQ(valuepairs.first, 2);
   EXPECT_EQ(valuepairs.second[0].id, 1);
   EXPECT_EQ(valuepairs.second[0].value, 128);
@@ -113,15 +90,14 @@ TEST_P(CombinationsTest, TestFnAndHashtableCombination) {
   // Get input.
   const auto [test_name, ht_name] = GetParam();
   const auto hashtable_size = absl::GetFlag(FLAGS_hashtable_size);
+  PLOG_INFO << "Running test " << test_name << " with hashtable " << ht_name;
 
   // Get test function.
   const auto test_fn = [test_name]() -> void (*)(kmercounter::BaseHashTable*) {
     if (test_name == NO_PREFETCH_TEST)
       return kmercounter::no_prefetch_test;
-    else if (test_name == UNIT_FILL_TEST)
-      return kmercounter::single_insert_test;
-    else if (test_name == OFF_BY_ONE_TEST)
-      return kmercounter::off_by_one_test;
+    else if (test_name == SIMPLE_BATCH_QUERY_TEST)
+      return kmercounter::simple_batch_query_test;
     else
       return nullptr;
   }();
@@ -129,7 +105,7 @@ TEST_P(CombinationsTest, TestFnAndHashtableCombination) {
 
   // Get hashtable.
   const auto ht = std::unique_ptr<kmercounter::BaseHashTable>([ht_name, hashtable_size]() -> kmercounter::BaseHashTable* {
-    if (ht_name == PARTITIONED_CAS_HT)
+    if (ht_name == PARTITIONED_HT)
       return new kmercounter::PartitionedHashStore<kmercounter::Item,
                                                    kmercounter::ItemQueue>{
           hashtable_size, 0};
@@ -143,6 +119,9 @@ TEST_P(CombinationsTest, TestFnAndHashtableCombination) {
   ASSERT_NE(ht, nullptr) << "Invalid hashtable type: " << ht_name;
 
   // Run test and clean up.
+  if (ht_name == PARTITIONED_HT && test_name == NO_PREFETCH_TEST) {
+    PLOG_INFO << "no_prefetch is not implemented in PartitionedHT. Skipping.";
+  }
   ASSERT_NO_THROW(test_fn(ht.get()));
 }
 
