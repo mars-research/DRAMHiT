88838be3d128b4f7baccc2cc6c6bf95ec12f8030
Tianjiao Huang <git@tjhu.dev>
2022-01-21 00:56:24 -0500

    make sure the github actions run all tests

--- auto-tests/test_lib.cpp
+++ auto-tests/test_lib.cpp
@@ -4,14 +4,14 @@
 
 #include "logger.h"
 
-ABSL_FLAG(int, hashtable_size, 1ull << 26,
+ABSL_FLAG(uint64_t, hashtable_size, 1ull << 11,
           "size of hashtable.");
-ABSL_FLAG(int, test_size, 1ull << 12,
+ABSL_FLAG(uint64_t, test_size, 1ull << 10,
           "size of test(number of insertions/lookup).");
 
 int main(int argc, char **argv) {
+  initializeLogger();
   ::testing::InitGoogleTest(&argc, argv);
   absl::ParseCommandLine(argc, argv);
-  initializeLogger();
   return RUN_ALL_TESTS();
 }
