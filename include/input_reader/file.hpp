136641e5b9e6ffe9fc3e56df6e5871dbed1f7485
Tianjiao Huang <git@tjhu.dev>
2022-01-23 00:37:28 -0500

    fix PartitionedFileReader missing lines

--- include/input_reader/file.hpp
+++ include/input_reader/file.hpp
@@ -1,12 +1,12 @@
 #ifndef INPUT_READER_FILE_HPP
 #define INPUT_READER_FILE_HPP
 
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <string_view>
+#include <iostream>
 #include <limits>
 #include <memory>
+#include <string>
+#include <string_view>
 #include <plog/Log.h>
 
 #include "input_reader_base.hpp"
@@ -18,18 +18,22 @@ namespace input_reader {
 /// The file is sliced up evenly among the partitions
 class PartitionedFileReader : public InputReader<std::string> {
  public:
-  PartitionedFileReader(std::string_view filename, uint64_t part_id, uint64_t num_parts) 
-  :
-  PartitionedFileReader(std::make_unique<std::ifstream>(filename.data()), part_id, num_parts)
-  {
-    PLOG_FATAL_IF(this->input_file->fail()) << "Failed to open file " << filename;
+  PartitionedFileReader(std::string_view filename, uint64_t part_id,
+                        uint64_t num_parts)
+      : PartitionedFileReader(std::make_unique<std::ifstream>(filename.data()),
+                              part_id, num_parts) {
+    PLOG_FATAL_IF(this->input_file->fail())
+        << "Failed to open file " << filename;
   }
 
-  PartitionedFileReader(std::unique_ptr<std::istream> input_file, uint64_t part_id, uint64_t num_parts)
-      : input_file(std::move(input_file)) {
-
+  PartitionedFileReader(std::unique_ptr<std::istream> input_file,
+                        uint64_t part_id, uint64_t num_parts)
+      : input_file(std::move(input_file)),
+        part_id_(part_id),
+        num_parts_(num_parts) {
     // Get the size of the file and calculate the range of this partition.
-    // We are doing it here for now because I don't want to mess with the parameter passing.
+    // We are doing it here for now because I don't want to mess with the
+    // parameter passing.
     this->input_file->seekg(0, std::ios::end);
     const uint64_t file_size = this->input_file->tellg();
     this->input_file->seekg(0);
@@ -38,18 +42,19 @@ class PartitionedFileReader : public InputReader<std::string> {
       if (part_id == num_parts) {
         return std::numeric_limits<uint64_t>::max();
       } else {
-        // This is same as `file_size` but I am worrying about error cause by floating precision.
+        // This is same as `file_size` but I am worrying about error cause by
+        // floating precision.
         return (double)file_size / num_parts * (part_id + 1);
       }
     }();
-    PLOG_DEBUG << part_id << "/" << num_parts << ": " << part_start << ", " << part_end;
+    PLOG_DEBUG << part_id << "/" << num_parts << ": " << part_start << ", "
+               << part_end;
 
     // If `start` is not 0 and `start - 1` is not a newline,
     // seek to the next newline and start reading from there.
     if (part_start != 0) {
       this->input_file->seekg(part_start - 1);
-      char c;
-      *this->input_file >> c;
+      char c = this->input_file->get();
       if (c != '\n') {
         std::string tmp;
         std::getline(*this->input_file, tmp);
@@ -59,20 +64,17 @@ class PartitionedFileReader : public InputReader<std::string> {
 
   std::optional<std::string> next() override {
     std::string str;
-    if (((uint64_t)this->input_file->tellg() >= this->part_end) || !std::getline(*this->input_file, str)) {
+    if (((uint64_t)this->input_file->tellg() >= this->part_end) ||
+        !std::getline(*this->input_file, str)) {
       return std::nullopt;
     } else {
       return str;
     }
   }
 
-  uint64_t num_parts() {
-    return num_parts_;
-  }
+  uint64_t num_parts() { return num_parts_; }
 
-  uint64_t part_id() {
-    return part_id_;
-  }
+  uint64_t part_id() { return part_id_; }
 
  private:
   std::unique_ptr<std::istream> input_file;
@@ -83,9 +85,11 @@ class PartitionedFileReader : public InputReader<std::string> {
 
 /// Read one line at a time.
 class FileReader : public PartitionedFileReader {
-public:
-  FileReader(std::string_view filename) : PartitionedFileReader(filename, 0, 1) {}
-  FileReader(std::unique_ptr<std::istream> input_file) : PartitionedFileReader(std::move(input_file), 0, 1) {}
+ public:
+  FileReader(std::string_view filename)
+      : PartitionedFileReader(filename, 0, 1) {}
+  FileReader(std::unique_ptr<std::istream> input_file)
+      : PartitionedFileReader(std::move(input_file), 0, 1) {}
 };
 
 }  // namespace input_reader
