03e0f20b40f37e74b2fe8138da9779655494c485
Tianjiao Huang <git@tjhu.dev>
2022-02-02 23:50:18 -0500

    cb: add push test

--- auto-tests/utils/circular_buffer_test.cpp
+++ auto-tests/utils/circular_buffer_test.cpp
@@ -2,3 +2,42 @@
 
 #include "utils/circular_buffer.hpp"
 
+
+namespace kmercounter {
+namespace input_reader {
+namespace {
+
+TEST(CircularBufferTest, PushTest) {
+  {
+    CircularBufferU8<3> cb;
+    EXPECT_EQ(cb.offset(), 0);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 1);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 2);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 0);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 1);
+  }
+
+  {
+    CircularBufferU8<4> cb;
+    EXPECT_EQ(cb.offset(), 0);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 1);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 2);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 3);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 0);
+    cb.push(0);
+    EXPECT_EQ(cb.offset(), 1);
+  }
+}
+
+
+} // namespace
+} // namespace input_reader
+} // namespace kmercounter
--- include/utils/circular_buffer.hpp
+++ include/utils/circular_buffer.hpp
@@ -2,17 +2,44 @@
 #define UTILS_CIRCULAR_BUFFER_HPP
 
 #include <array>
+#include <cstring>
 
-/// A always-full circular buffer.
-template <size_t N>
+namespace kmercounter {
+/// An always-full circular buffer.
+template <typename T, size_t N>
 class CircularBuffer {
 public:
-  CircularBuffer() = default;
-  CircularBuffer(const std::array<uint8_t, N>& data) : data_(data), offset_(N - 1) {}
+  CircularBuffer() : offset_(0) {}
+  CircularBuffer(const std::array<T, N>& data) : data_(data), offset_(N - 1) {}
+
+  void push(T data) {
+    data_[offset_] = data;
+    offset_ = (offset_ + 1) % N;
+  }
+
+  void copy_to(T *dst) {
+    // Copy first half.
+    const auto first_half_size = N - offset_ - 1;
+    memcpy(dst, data_.data() + offset, first_half_size * sizeof(T));
+    // Copy second half.
+    const auto second_half_size = N - first_half_size;
+    memcpy(dst + first_half_size, data_.data(), second_half_size * sizeof(T));
+  }
+
+  uint32_t offset() {
+    return offset_;
+  }
 
 private:
-  std::array<uint8_t, N> data_;
+  std::array<T, N> data_;
+  /// Indicate the current tail
+  /// Range: [0, N)
   uint32_t offset_;
+  static_assert(N < (1 << (sizeof(offset_)*8 - 1)), "N is too large; not addressable by offset_");
 };
 
+template <size_t N>
+using CircularBufferU8 = CircularBuffer<uint8_t, N>;
+
+} // namespace kmercounter
 #endif // UTILS_CIRCULAR_BUFFER_HPP
