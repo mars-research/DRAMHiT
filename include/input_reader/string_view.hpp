70326328efd3641d55b7df357d3509afbfc9ecc1
Tianjiao Huang <git@tjhu.dev>
2022-02-06 19:45:50 -0500

    add StringViewReader

---
+++ include/input_reader/string_view.hpp
@@ -0,0 +1,31 @@
+#ifndef INPUT_READER_STRING_VIEW_HPP
+#define INPUT_READER_STRING_VIEW_HPP
+
+#include "input_reader_base.hpp"
+
+#include <string_view>
+
+#include "input_reader/iterator.hpp"
+
+namespace kmercounter {
+namespace input_reader {
+template<class T>
+class StringViewReader : public InputReader<T> {
+public:
+    StringViewReader(std::basic_string_view<T> data) : data_(data), iter_(data_.begin(), data_.end()) {}
+
+    bool next(T *data) override {
+        return iter_.next(data);
+    }
+
+    size_t size() {
+        return data_.size();
+    }
+
+private:
+    std::basic_string_view<T> data_;
+    IterReader<decltype(data_.begin()), T> iter_;
+};
+} // namespace input_reader
+} // namespace kmercounter
+#endif // INPUT_READER_STRING_VIEW_HPP
