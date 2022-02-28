4213cc37599588211e5a8f7275febdf12b8a96c1
Tianjiao Huang <git@tjhu.dev>
2022-02-28 15:43:47 -0500

    add cross check

--- scripts/estimate_zipf.py
+++ scripts/estimate_zipf.py
@@ -2,7 +2,7 @@ from scipy.optimize import minimize
 from scipy import special
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+from collections import Counter
 
 size = 100
 
@@ -14,13 +14,27 @@ def estimate_zipf(arr):
   min_f = lambda s: np.sum(np.power(np.log(p) - lzipf(s[0]), 2))
   return minimize(min_f, np.array([0.5]))
 
-arr = np.fromfile('../hist', dtype=int, sep='\n')[:size]
-estimated = estimate_zipf(arr)
-print(estimated)
+def cross_check(a):
+  arr = np.random.zipf(a, size * 10000)
+  arr = Counter(arr)
+  arr = np.array([x[1] for x in arr.most_common()])
+  estimated_a = estimate_zipf(arr).x[0]
+  print(f"Expected: {a:.2f}, actual: {estimated_a:.2f}")
 
-a = estimated.x[0]
-x = np.arange(1., float(size))
-y = x**(-a) / special.zetac(a)
-plt.plot(x, y/max(y), linewidth=2, color='r')
-plt.bar(np.arange(1,size+1), arr/max(arr))
-plt.savefig('hist.jpg')
+
+def plot_zipf(file):
+  arr = np.fromfile(file, dtype=int, sep='\n')[:size]
+  estimated = estimate_zipf(arr)
+  a = estimated.x[0]
+  print(f'a is estimated to be {a:.2f}')
+
+  x = np.arange(1., float(size))
+  y = x**(-a) / special.zetac(a)
+  plt.plot(x, y/max(y), linewidth=2, color='r', label='estimated zipf')
+  plt.bar(np.arange(1,size+1), arr/max(arr), label='input')
+  plt.savefig('hist.jpg')
+
+if __name__ == "__main__":
+  plot_zipf('../hist')
+  cross_check(1.5)
+  cross_check(2)
