c6feb8f932d66e8aeebfebae22f50ea222e75b77
Tianjiao Huang <git@tjhu.dev>
2022-03-07 13:44:37 -0500

    add reservoir

--- auto-tests/input_reader/reservoir_test.cpp
+++ auto-tests/input_reader/reservoir_test.cpp
@@ -11,12 +11,11 @@ namespace input_reader {
 
 namespace {
 
-TEST(ContainerReaderTest, SimpleTest) {
+TEST(RangeReader, SimpleTest) {
   std::vector vec{1, 3, 5, 7};
   {
     auto reader = std::make_unique<RangeReader<decltype(vec.begin())>>(vec.begin(), vec.end());
-    auto reservoir = std::make_unique<Reservoir<int>>(std::move(reader));
-    EXPECT_EQ(4, reader_size(std::move(reservoir)));
+    EXPECT_EQ(4, reader_size(std::move(reader)));
   }
 
   vec.push_back(123);
@@ -41,8 +40,57 @@ TEST(ContainerReaderTest, SimpleTest) {
 
 }
 
+TEST(VecReader, SimpleTest) {
+  std::vector vec{1, 3, 5, 7};
+  {
+    auto reader = std::make_unique<VecReader<int>>(vec);
+    for (auto expected_val : vec) {
+      int val;
+      EXPECT_TRUE(reader->next(&val));
+      EXPECT_EQ(expected_val, val);
+    }
+    int val;
+    EXPECT_FALSE(reader->next(&val));
+  }
 
+  vec.push_back(123);
+  {
+    auto reader = std::make_unique<VecReader<int>>(vec);
+    for (auto expected_val : vec) {
+      int val;
+      EXPECT_TRUE(reader->next(&val));
+      EXPECT_EQ(expected_val, val);
+    }
+    int val;
+    EXPECT_FALSE(reader->next(&val));
+  }
+}
 
+TEST(ReserviorReader, SimpleTest) {
+  std::vector vec{1, 3, 5, 7};
+  {
+    auto reader = std::make_unique<VecReader<int>>(vec);
+    auto reservoir = std::make_unique<Reservoir<int>>(std::move(reader));
+    int val;
+    for (auto expected_val : vec) {
+      EXPECT_TRUE(reservoir->next(&val));
+      EXPECT_EQ(expected_val, val);
+    }
+    EXPECT_FALSE(reservoir->next(&val));
+  }
+
+  vec.push_back(123);
+  {
+    auto reader = std::make_unique<VecReader<int>>(vec);
+    auto reservoir = std::make_unique<Reservoir<int>>(std::move(reader));
+    int val;
+    for (auto expected_val : vec) {
+      EXPECT_TRUE(reservoir->next(&val));
+      EXPECT_EQ(expected_val, val);
+    }
+    EXPECT_FALSE(reservoir->next(&val));
+  }
+}
 
 } // namespace
 } // namespace input_reader
