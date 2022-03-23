8f13a0e569968006416c67c6c3cf6a064feebf20
Tianjiao Huang <git@tjhu.dev>
2022-03-23 14:54:33 -0400

    add script that plots time vs K

---
+++ scripts/graph/time_vs_k.py
@@ -0,0 +1,66 @@
+import pathlib
+import os
+import shutil
+import re
+import subprocess
+
+def run_subprocess(cmd):
+    print(f'Running <{cmd}>')
+    subprocess.run(cmd, check=True, shell=True)
+
+def get_avg_insert_cycle(file):
+    with open(file, 'r') as f:
+        text = f.read()
+        cycles = int(re.search(
+            r'Average  : .* cycles \(.* ms\) for .* insertions \((?P<cycles>(?:[0-9]*)) cycles/insert\) \(fill = .* %\)', text)['cycles'])
+        return cycles
+            
+
+def run_test(extra_build_args, extra_kvstore_args, outpath):
+    # Config build
+    build_dir = f'build/tmp'
+    build_cmd = f'cmake -S . -B {build_dir} -G Ninja {extra_build_args} > {outpath}.cmake.log'
+    run_subprocess(build_cmd)
+    
+    # Build
+    run_subprocess(f'ninja -C {build_dir} > {outpath}.build.log')
+
+    # Run kvstore
+    run_subprocess(f'{build_dir}/kvstore {extra_kvstore_args} > {outpath}')
+
+    # Extract cycle count
+    return get_avg_insert_cycle(outpath)
+
+if __name__ == '__main__':
+    Ks = list(range(4, 32))
+    OUTPATH = 'out'
+    FASTQ_FILE = '../memfs/SRR077487.2.fastq'
+
+    # Run CASHT++
+    casht_cycles = []
+    test_type = 'CASHT'
+    run_subprocess(f'mkdir -p {OUTPATH}/{test_type}')
+    for K in Ks:
+        print(f'Running {test_type} K={K}')
+        build_args = f'-DBQUEUE=OFF -DKMER_LEN={K}'
+        kvstore_args = f'--num-threads=64 --mode=6 --ht-type=3 --numa-split=1 --in-file={FASTQ_FILE}'
+        rtn = run_test(build_args, kvstore_args, f'{OUTPATH}/{test_type}/{K}.log')
+        print(f'cycles {rtn}')
+        casht_cycles.append(rtn)
+    print(f'{test_type}: {casht_cycles}')
+    
+
+    # Run PartitionedHT
+    partitionedht_cycles = []
+    test_type = 'CASHT'
+    run_subprocess(f'mkdir -p {OUTPATH}/{test_type}')
+    for K in Ks:
+        print(f'Running {test_type} K={K}')
+        build_args = f'-DBQUEUE=ON -DKMER_LEN={K}'
+        kvstore_args = f'--nprod 32 --ncons 32 --mode=8 --ht-type=1 --in-file={FASTQ_FILE}'
+        rtn = run_test(build_args, kvstore_args, f'{OUTPATH}/{test_type}/{K}.log')
+        print(f'cycles {rtn}')
+        partitionedht_cycles.append(rtn)
+    print(f'{test_type}: {partitionedht_cycles}')
+
+    
