621757e92bbb0a1046fd3523510f99099fd61841
Tianjiao Huang <git@tjhu.dev>
2022-06-20 19:52:22 -0400

    simplify default.nix and add python packages for eval plotting

---
+++ build_package.nix
@@ -0,0 +1,57 @@
+# This file contains all the packages for building the repo.
+# Change flake.nix to modify the dev shell.
+
+{
+  srcPath ? ./.,
+  lib,
+  stdenv,
+  cmakeFlags ? [],
+  gcc11, cmake, ninja,
+  abseil-cpp,
+  numactl,
+  zlib,
+  boost,
+  gtest,
+  capstone,
+}: let
+  abseil-cpp-17 = abseil-cpp.override {
+    cxxStandard = "17";
+  };
+in stdenv.mkDerivation {
+  name = "kvstore";
+  version = "0.1.0";
+
+  inherit cmakeFlags;
+
+  src = lib.cleanSourceWith {
+    filter = name: type: !(type == "directory" && baseNameOf name == "build");
+    src = lib.cleanSourceWith {
+      filter = lib.cleanSourceFilter;
+      src = srcPath;
+    };
+  };
+
+  nativeBuildInputs = [ 
+    gcc11
+    cmake
+    ninja
+  ];
+
+  buildInputs = [
+    abseil-cpp-17
+    numactl
+    zlib
+    boost
+    gtest
+    capstone
+  ];
+
+  NIX_CFLAGS_COMPILE = "-march=native";
+  
+  installPhase = ''
+    mkdir -p $out/bin
+    cp kvstore $out/bin/
+    find auto-tests/ -executable -type f -exec cp {} $out/bin/ \;
+  '';
+}
+
