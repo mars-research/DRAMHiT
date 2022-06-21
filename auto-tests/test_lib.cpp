f9d1a8e1e001eda8caae109fddd0a29c657b0e3d
Tianjiao Huang <git@tjhu.dev>
2022-06-20 20:03:32 -0400

    suppress debug logging in unittests

--- auto-tests/test_lib.cpp
+++ auto-tests/test_lib.cpp
@@ -4,14 +4,18 @@
 
 #include "logger.h"
 
-ABSL_FLAG(uint64_t, hashtable_size, 1ull << 11,
-          "size of hashtable.");
+ABSL_FLAG(uint64_t, hashtable_size, 1ull << 11, "size of hashtable.");
 ABSL_FLAG(uint64_t, test_size, 1ull << 10,
           "size of test(number of insertions/lookup).");
+ABSL_FLAG(int, log_level, plog::info,
+          "Log level of plog. See "
+          "https://github.com/SergiusTheBest/plog/blob/master/include/plog/Severity.h"
+          "for more options");
 
 int main(int argc, char **argv) {
   initializeLogger();
   ::testing::InitGoogleTest(&argc, argv);
   absl::ParseCommandLine(argc, argv);
+  plog::get()->setMaxSeverity(plog::info);
   return RUN_ALL_TESTS();
 }
