384b37ac1df0db874249eeb2cdd59897e752b66e
Vikram Narayanan <vikram186@gmail.com>
2022-10-19 19:17:15 -0400

    Add fastqkmerreader K selection

--- include/input_reader/fastq.hpp
+++ include/input_reader/fastq.hpp
@@ -142,6 +142,28 @@ class FastqKMerPreloadReader : public InputReaderU64 {
   KMerReader<K> reader_;
 };
 
+/// Helper for instantiating a `FastqKMerReader` from a runtime `K`.
+template <uint32_t CurrentK=DNAKMer<1>::MAX_K, typename... Args>
+std::unique_ptr<InputReaderU64> MakeFastqKMerReader(uint32_t K, Args&&... args) {
+  // Safety check.
+  if (K > DNAKMer<1>::MAX_K || K < 1) {
+    PLOG_FATAL << "K=" << K << " is not a valid value";
+    return nullptr;
+  }
+
+  // Found the right K.
+  if (K == CurrentK) {
+    return std::make_unique<FastqKMerReader<CurrentK>>(std::forward<Args>(args)...);
+  }
+
+  // Recurse until we found the right K.
+  // Constexpr is necessary here; the compiler will go into an infinite loop otherwise.
+  if constexpr (CurrentK > 1) {
+    return MakeFastqKMerReader<CurrentK-1, Args...>(K, std::forward<Args>(args)...);
+  }
+  return nullptr;
+}
+
 /// Helper for instantiating a `FastqKMerPreloadReader` from a runtime `K`.
 template <uint32_t CurrentK=DNAKMer<1>::MAX_K, typename... Args>
 std::unique_ptr<InputReaderU64> MakeFastqKMerPreloadReader(uint32_t K, Args&&... args) {
