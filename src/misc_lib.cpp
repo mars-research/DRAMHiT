c38ebca530b3eeee29a963b5f8ad011ff6396a56
Vikram Narayanan <vikram186@gmail.com>
2023-02-19 21:32:11 -0500

    refactor memory dist to numa nodes

--- src/misc_lib.cpp
+++ src/misc_lib.cpp
@@ -41,3 +41,45 @@ uint64_t __attribute__((optimize("O0"))) touchpages(char *fmap, size_t sz) {
   for (uint64_t i = 0; i < sz; i += PAGE_SIZE) sum += fmap[i];
   return sum;
 }
+
+#if 1
+
+#include "numa.hpp"
+#include "types.hpp"
+#include <numaif.h>
+
+namespace kmercounter {
+extern Configuration config;
+
+void distribute_mem_to_nodes(void *addr, size_t alloc_sz) {
+    void *_addr = addr;
+    Numa *n = new Numa();
+    auto num_nodes = n->get_num_nodes();
+    size_t split_den = std::min(config.num_threads, static_cast<uint32_t>(num_nodes));
+    size_t len_split = ((alloc_sz / split_den) + (PAGE_SIZE - 1)) & ~(PAGE_SIZE - 1);
+
+    struct bitmask *b = numa_allocate_nodemask();
+
+    PLOGV.printf("nodemask %lx | all_nodes %lx", *b->maskp, *numa_all_nodes_ptr->maskp);
+    for (int i = 0; i < split_den; i++) {
+      void *addr_split = (char *)_addr + len_split * i;
+      unsigned long nodemask[4096] = {0};
+
+      nodemask[0] = 1 << i;
+
+      PLOGV.printf("Moving half the memory to node %lu", nodemask[0]);
+
+      long ret = mbind(addr_split, len_split, MPOL_BIND, nodemask, 4096,
+          MPOL_MF_MOVE | MPOL_MF_STRICT);
+
+      //PLOGV.printf("mmap_addr %p | len %zu", _addr, capacity * sizeof(T));
+      PLOGV.printf("calling mbind with addr %p | len %zu | nodemask %p", addr_split,
+          len_split, nodemask);
+      if (ret < 0) {
+        perror("mbind");
+        PLOGE.printf("mbind ret %ld | errno %d", ret, errno);
+      }
+    }
+}
+} // namespace
+#endif
