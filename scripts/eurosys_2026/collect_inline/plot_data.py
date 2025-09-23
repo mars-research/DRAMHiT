50334c68e0ae69b69bafa3c943d8b6547123ecec
jerryidk <jerryzhang19257@gmail.com>
2025-09-23 12:25:36 -0600

    save

--- scripts/eurosys_2026/collect_inline/plot_data.py
+++ scripts/eurosys_2026/collect_inline/plot_data.py
@@ -8,6 +8,8 @@ import pandas as pd
 from matplotlib.lines import Line2D
 
 
+
+
 def plot_json(json_file, output_file):
     # Load JSON data
     with open(json_file, "r") as f:
@@ -15,19 +17,23 @@ def plot_json(json_file, output_file):
 
     # Convert to pandas DataFrame
     df = pd.DataFrame(data)
-    
     df = pd.json_normalize(data, sep='.')
-    
+
+    # Filter on numa_policy
     df = df[df["run_cfg.numa_policy"] == 4]
+
+    # Derived metrics
     df["normalized_ops"] = df["uops_dispatched.port_2_3_10"] / df["find_ops"]
+    df["relative_mem_uops"] = df["uops_dispatched.port_2_3_10"] / df["uops_issued.any"]
 
     # Set Seaborn style
     sns.set_theme()
-    
+
     row = 1
-    col = 2
-    fig, axes = plt.subplots(row, col, figsize=(16, 8))    
-        
+    col = 3
+    fig, axes = plt.subplots(row, col, figsize=(12, 3))
+
+    # Plot 1: Throughput
     ax = axes[0]
     sns.lineplot(
         data=df,
@@ -42,6 +48,7 @@ def plot_json(json_file, output_file):
     ax.set_xlabel("Fill Factor")
     ax.set_ylabel("Find Mops")
 
+    # Plot 2: Normalized ops
     ax = axes[1]
     sns.lineplot(
         data=df,
@@ -52,28 +59,40 @@ def plot_json(json_file, output_file):
         ax=ax,
         legend=False
     )
-    ax.set_title("Single Socket - PMU Counter")
+    ax.set_title("Single Socket - mem uops/find")
     ax.set_xlabel("Fill Factor")
     ax.set_ylabel("Memory Uops/Find")
 
-    
+    # Plot 3: Relative memory uops
+    ax = axes[2]
+    sns.lineplot(
+        data=df,
+        x="run_cfg.fill_factor",
+        y="relative_mem_uops",
+        hue="identifier",
+        marker="o",
+        ax=ax,
+        legend=False
+    )
+    ax.set_title("Single Socket - Relative Mem Uops")
+    ax.set_xlabel("Fill Factor")
+    ax.set_ylabel("Mem Uops / All Uops")
+
+    # Legend
     unique_ids = df["identifier"].unique()
     palette = sns.color_palette(n_colors=len(unique_ids))
-
     custom_lines = [
         Line2D([0], [0], color=palette[i], marker="o", label=uid)
         for i, uid in enumerate(unique_ids)
     ]
-
     fig.legend(
         fontsize=8,
         handles=custom_lines,
         loc="upper center",
-        ncol=2
+        ncol=3
     )
 
-    plt.tight_layout(rect=[0, 0, 1, 0.95])
-
+    plt.tight_layout(rect=[0, 0, 1, 0.90])
     plt.savefig(output_file, dpi=300)
     print(f"[OK] Plots saved to {output_file}")
 
@@ -82,8 +101,8 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python plot_dramhit.py <input.json> <output.png>")
         sys.exit(1)
+        
 
     json_file = sys.argv[1]
     output_file = sys.argv[2]
     plot_json(json_file, output_file)
-
