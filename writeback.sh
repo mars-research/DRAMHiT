2e30d1fe3cb4c9a58ba3519080389529a2a83778
jerry <jerryzhang19257@gmail.com>
2025-09-30 15:29:09 -0600

    clean up repo

--- writeback.sh
+++
@@ -1,7 +0,0 @@
- perf=/usr/bin/perf
- rm a.out
-gcc -O1 -pthread -lnuma writeback.c -I/opt/intel/oneapi/vtune/latest/include /opt/intel/oneapi/vtune/latest/lib64/libittnotify.a
- sudo $perf stat -I 1000 -e "unc_m2m_directory_lookup.state_a, unc_m2m_directory_lookup.state_i, unc_m2m_directory_lookup.state_s" -- ./a.out
-
-
-# gcc -O1 -pthread -lnuma writeback.c -I/opt/intel/oneapi/vtune/latest/include /opt/intel/oneapi/vtune/latest/lib64/libittnotify.a
