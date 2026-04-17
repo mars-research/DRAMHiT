e8107d268bd759cb52ebe65d0b49656f0c50bf98
jerryidk <jerryidk@dramhit.jerryidk-298808.redshift-pg0.utah.cloudlab.us>
2026-04-16 19:55:34 -0600

    all

---
+++ include/all_ht_types.hpp
@@ -0,0 +1,22 @@
+#ifndef HASHTABLES_ALL_HPP
+#define HASHTABLES_ALL_HPP
+
+#include "hashtables/array_kht.hpp"
+#include "hashtables/cas23_kht.hpp"
+#include "hashtables/cas_kht.hpp"
+
+#ifdef GROWT
+#include "hashtables/growt_kht.hpp"
+#include "hashtables/tbb_kht.hpp"
+#endif
+
+#ifdef CLHT
+#include "hashtables/clht_kht.hpp"
+#endif
+
+#ifdef PART_ID
+#include "hashtables/multi_kht.hpp"
+#include "hashtables/simple_kht.hpp"
+#endif
+
+#endif
